8d3ab08c696d853868fd387f56cb4c9fdeb10713
Majd <majd-louka@hotmail.com>
2024-06-04 00:09:39 +0300

    added the extras and completed storing images (hopefully)

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,6 @@ Route::middleware('auth:sanctum')->group(function ()
 });
 //f*ck postman
 
-
 Route::controller(AuthController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login_as_customer', 'login_as_customer');
